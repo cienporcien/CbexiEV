e1565f9a3b4d21f675a2258fc8d4dca5459901bc
Roger Bedell <rogerbedell@hotmail.com>
2024-02-18 20:11:14 +0100

    Now compiles and links in evlibiso15118. Started implementing, but just a little for testing.

--- ev/ISO15118_evImpl.cpp
+++ ev/ISO15118_evImpl.cpp
@@ -7,19 +7,68 @@
 #include <eviso15118/io/logging.hpp>
 #include <eviso15118/session/feedback.hpp>
 #include <eviso15118/session/logger.hpp>
-//#include <eviso15118/tbd_controller.hpp>
+#include <eviso15118/tbd_controller.hpp>
+
+eviso15118::TbdConfig tbd_config_20;
+std::unique_ptr<SessionLogger> session_logger;
+eviso15118::session::feedback::Callbacks callbacks_20;
+std::unique_ptr<eviso15118::TbdController> controller_20;
 
 namespace module {
 namespace ev {
 
+static std::filesystem::path get_cert_path(const std::filesystem::path& initial_path, const std::string& config_path) {
+    if (config_path.empty()) {
+        return initial_path;
+    }
+
+    if (*config_path.begin() == '/') {
+        return config_path;
+    } else {
+        return initial_path / config_path;
+    }
+}
+
+static eviso15118::config::TlsNegotiationStrategy convert_tls_negotiation_strategy(const std::string& strategy) {
+    using Strategy = eviso15118::config::TlsNegotiationStrategy;
+    if (strategy.compare("ACCEPT_CLIENT_OFFER") == 0) {
+        return Strategy::ACCEPT_CLIENT_OFFER;
+    } else if (strategy.compare("ENFORCE_TLS") == 0) {
+        return Strategy::ENFORCE_TLS;
+    } else if (strategy.compare("ENFORCE_NO_TLS") == 0) {
+        return Strategy::ENFORCE_NO_TLS;
+    } else {
+        EVLOG_AND_THROW(Everest::EverestConfigError("Invalid choice for tls_negotiation_strategy: " + strategy));
+        // better safe than sorry
+    }
+}
+
 void ISO15118_evImpl::init() {
+    // RDB - No logging path in the ev interface...
+    // setup logging routine
+    eviso15118::io::set_logging_callback([](const std::string& msg) { EVLOG_info << msg; });
+
+    // RDB use a temp folder instead
+    std::filesystem::path p = std::filesystem::temp_directory_path();
+    session_logger = std::make_unique<SessionLogger>(p.string());
+
+    //controller_20 = std::make_unique<eviso15118::TbdController>(tbd_config_20, callbacks_20);
 }
 
 void ISO15118_evImpl::ready() {
+    //RDB start looping waiting for messages and commands to come in
+    // while (true) {
+    //     try {
+    //         controller_20->loop();
+    //     } catch (const std::exception& e) {
+    //         EVLOG_error << "D20 EV crashed: " << e.what();
+    //         //publish_dlink_error(nullptr);
+    //     }
+    // }
 }
 
 bool ISO15118_evImpl::handle_start_charging(std::string& PaymentOption, std::string& EnergyTransferMode) {
-    // your code for cmd start_charging goes here
+    // Start sending wireless SDP messages and react to the response(s)
     return true;
 }
 
